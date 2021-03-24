1b684eb633f9cf1b619a42c9f54503ec4100ed13
Joy Atsu Dzakpasu <jasonjoywinmorre@gmail.com>
2021-03-24 12:11:04 +0000

    fixed redis configuration errors

--- src/main/java/turntabl/io/reporting_service/ReportingServiceConfig.java
+++ src/main/java/turntabl/io/reporting_service/ReportingServiceConfig.java
@@ -1,7 +1,17 @@
 package turntabl.io.reporting_service;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import turntabl.io.reporting_service.listener.ReportingListener;
 
+@Configuration
 public class ReportingServiceConfig {
     @Bean
     public JedisConnectionFactory connectionFactory(){
@@ -22,17 +32,17 @@ public class ReportingServiceConfig {
     }
 
     @Bean
-    public ChannelTopic topic() {return new ChannelTopic("pubsub:message-channel");}
+    public ChannelTopic topic() {return new ChannelTopic("reporting-service");}
 
-    @Bean
-    Publisher redisPublisher(){
-        return new Publisher(template(),topic());
-    }
+//    @Bean
+//    Publisher redisPublisher(){
+//        return new Publisher(template(),topic());
+//    }
 
     @Bean
     public MessageListenerAdapter messageListenerAdapter(){
 //        pass the receiver class to the message listener adapter
-        return new MessageListenerAdapter(new TradeListener());
+        return new MessageListenerAdapter(new ReportingListener());
     }
 
     @Bean
